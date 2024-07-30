2d960a6263e5c6c46361914b2f64994b020b8ee2
javiercoronadonarvaez <j.coronado.narvaez@accenture.com>
2024-07-30 17:36:58 -0600

    Complemented test from part1 with latest concepts

--- part1/test/src/App.jsx
+++ part1/test/src/App.jsx
@@ -1,12 +1,16 @@
 import PropTypes from 'prop-types';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
-const Hello = (props) => {
-  console.log(props)
+const Hello = ({ name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age
+
   return (
     <div>
       <p>
-        Hello {props.name}, you are {props.age} years old
+        Hello {name},  you are {age} years old
+      </p>
+      <p>
+        You are were probably born in {bornYear()}
       </p>
     </div>
   )
@@ -25,20 +29,104 @@ const Footer = () => {
   )
 }
 
+const Display = ({ counter }) => {
+  return (
+    <div>{counter}</div>
+  )
+}
+
+Display.propTypes = {
+  counter: PropTypes.number.isRequired
+};
+
+const Button = ({ onSmash, functionDescription } ) => {
+  return (
+    <button onClick={onSmash}>{functionDescription}</button>
+  )
+}
+
+const History = ({ allClicks }) => {
+  if (allClicks.length === 0) {
+    return (
+      <div>
+      the app is used by pressing buttons bro
+      </div>
+    )
+  }
+  return (
+    <div>
+    Button Press History: {allClicks.join(' ')}
+    </div>
+  )
+}
+
 const App = () => {
   const [currentTime, setCurrentTime] = useState(new Date())
+  const [counter, setCounter] = useState(0)
   const a = 15
   const b = 20
   const name = 'Megan'
   const age = 10
 
+  /*
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
+
+  const handleLeftClick = () => setClicks({...clicks, left: clicks.left + 1})
+
+  const handleRightClick = () => setClicks({...clicks, right: clicks.right + 1})
+  */
+
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    console.log('left before', left)
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    console.log('left after', left)
+    setTotal(updatedLeft + right)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    console.log('right before', right)
+    const updateRight = right + 1
+    setRight(updateRight)
+    console.log('right after', right)
+    setTotal(left + updateRight)
+  }
+
+  setTimeout(() => setCurrentTime(new Date()), 1000)
+  /*
   useEffect(() => {
     const timerID = setTimeout(() => {
       setCurrentTime(Date());
     }, 1000);
     return () => clearTimeout(timerID);
   })
-
+  */
+  const increaseByOne = () => setCounter(counter+1)
+  const decreaseByOne = () => setCounter(counter-1)
+  const restartCounter = () => setCounter(0)
+  const increaseByOneObject = {
+    'function': increaseByOne,
+    'description': '+'
+  }
+  const decreaseByOneObject = {
+    'function': decreaseByOne,
+    'description': '-'
+  }
+  const restartCounterObject = {
+    'function': restartCounter,
+    'description': 'Restart'
+  }
+  const buttonFunctions = [increaseByOneObject, decreaseByOneObject, restartCounterObject]
+  //const buttonFunctions = [increaseByOne, decreaseByOne, restartCounter]
   return (
     <>
       <h1>Greetings</h1>
@@ -48,6 +136,16 @@ const App = () => {
       <p>
         {a} plus {b} is {a + b}
       </p>
+      <Display counter={counter}/>
+      <>{buttonFunctions.map((content, index) => <Button key={index} onSmash={content.function} functionDescription={content.description} />)}</>
+      <div>
+        {left}
+        <button onClick={handleLeftClick}>left</button>
+        <button onClick={handleRightClick}>right</button>
+        {right}
+        <History allClicks={allClicks} />
+        <p>Total Clicks: {total} </p>
+      </div>
       <Footer />
     </>
   )
