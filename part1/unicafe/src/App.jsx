933eb721a8fc0494c929bf1f7c4750492aac16ba
javiercoronadonarvaez <j.coronado.narvaez@accenture.com>
2024-07-30 19:33:41 -0600

    Finished Exercise 1.8 from part1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,29 +1,33 @@
 import { useState } from 'react'
 
-const FeedbackDisplay = ({ statesList, updateFunction }) => {
+const FeedbackDisplay = ({ stateStringList, updateFunction }) => {
   return (
     <>
       <div style={{ color: 'blue', fontWeight: 'bold', fontSize: '20px' }}>
         Give Feedback
       </div>
       <div>
-        {statesList.map((state, index) => <Button key={index} label={state} setStateFunction={updateFunction} />)}
+        {stateStringList.map((state, index) => <Button key={index} label={state} setStateFunction={updateFunction} />)}
       </div>
     </>
   )
 }
 
-const StatisticsDisplay = ({ statesList, statisticsList }) => {
+const StatisticsDisplay = ({ stateStringList, stateList, statisticsStringList, statisticsList }) => {
   return (
     <div>
-      {statesList.map((state, index) => <p key={index}>{state}: {statisticsList[index]}</p>)}
+      {stateStringList.map((state, index) => <p key={index}>{state}: {stateList[index]}</p>)}
+      {statisticsStringList.map((state, index) => (
+        index < 2 ? (<p key={index}>{state}: {statisticsList[index]}</p>)
+        : (<p key={index}>{state}: {statisticsList[index]}%</p>)
+      ))}
     </div>
   )
 }
 
 const Button = ({ label, setStateFunction }) => {
   return (
-    <button onClick={setStateFunction(label)}>{label}</button>
+    <button onClick={() => setStateFunction(label)}>{label}</button>
   )
 }
 
@@ -32,28 +36,51 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const statesList = ['Good', 'Neutral', 'Bad']
-  const statisticsList = [good, neutral, bad]
+  const stateStringList = ['Good', 'Neutral', 'Bad']
+  const stateList = [good, neutral, bad]
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+  const statisticsStringList = ['All', 'Average', 'Positive']
+  const statisticsList = [all, average, positive]
 
-  const setStatisticsFunction = (label) => {
+  const setIndividualStateFunction = (label) => {
     if (label === 'Good') {
-      const totalGood = good + 1
-      return () => setGood(totalGood)
+      const newGood = good + 1
+      const newAll = newGood + neutral + bad
+      const newAverage = (1 * newGood -1 * bad) / newAll
+      const newPositive = (newGood / newAll) * 100
+      setGood(newGood)
+      setAll(newAll)
+      setAverage(newAverage)
+      setPositive(newPositive)
     }
     else if (label === 'Neutral') {
-      const totalNeutral = neutral + 1
-      return () => setNeutral(totalNeutral)
+      const newNeutral = neutral + 1
+      const newAll = good + newNeutral + bad
+      const newAverage = (1 * good -1 * bad) / newAll
+      const newPositive = (good / newAll) * 100
+      setNeutral(newNeutral)
+      setAll(newAll)
+      setAverage(newAverage)
+      setPositive(newPositive)
     }
     else {
-      const totalBad = bad + 1
-      return () => setBad(totalBad)
+      const newBad = bad + 1
+      const newAll = good + neutral + newBad
+      const newAverage = (1 * good -1 * newBad) / newAll
+      const newPositive = (good / newAll) * 100
+      setBad(newBad)
+      setAll(newAll)
+      setAverage(newAverage)
+      setPositive(newPositive)
     }
   }
 
   return (
     <div>
-      <FeedbackDisplay statesList={statesList} updateFunction={setStatisticsFunction} />
-      <StatisticsDisplay statesList={statesList} statisticsList={statisticsList} />
+      <FeedbackDisplay stateStringList={stateStringList} updateFunction={setIndividualStateFunction} />
+      <StatisticsDisplay stateStringList={stateStringList} stateList={stateList} statisticsStringList={statisticsStringList} statisticsList={statisticsList} />
     </div>
   )
 }
