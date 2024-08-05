fc664251b28b6b0b1cbb05fe2ffcc928cd024666
javiercoronadonarvaez <j.coronado.narvaez@accenture.com>
2024-08-05 13:07:33 -0600

    Updated test dev directory with lessons from Getting data from browser

--- part2/test/src/App.jsx
+++ part2/test/src/App.jsx
@@ -1,10 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('') 
   const [showAll, setShowAll] = useState(true)
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
+
+  console.log('render', notes.length, 'notes')
 
   const addNote = (event) => {
     event.preventDefault()
@@ -40,7 +55,7 @@ const App = (props) => {
       <ul>
         {notesToShow.map(note => <Note key={note.id} note={note} />)}
       </ul>
-      <form onSubmit={addNote}>
+      <form key='1' onSubmit={addNote}>
         <input 
           value={newNote}
           onChange={handleNoteChange}
