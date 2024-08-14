08d00b6489f0a3505050320d765dbd67dce293f7
javiercoronadonarvaez <j.coronado.narvaez@accenture.com>
2024-08-14 16:39:19 -0600

    Finished Exercise 4.4 from part4

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const sumOfLikes = blogs.reduce((sum, blog) => sum + blog.likes, 0);
+  return sumOfLikes;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
