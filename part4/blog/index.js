d34800ce0cffb92ed006fa064d40c8e453a0114b
javiercoronadonarvaez <j.coronado.narvaez@accenture.com>
2024-08-14 14:03:08 -0600

    Implemented blogs.js as a Routing controller

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -4,30 +4,33 @@ const cors = require("cors");
 const { info, error } = require("./utils/logger");
 const { MONGODB_URI, PORT } = require("./utils/config");
 const mongoose = require("mongoose");
-const Blog = require("./models/blog");
+const blogsRouter = require("./controllers/blogs");
 
 const mongoUrl = MONGODB_URI;
 mongoose
   .connect(mongoUrl)
   .then((result) => info(`Successful connection to MongoDB`))
-  .catch((error) => `Failed to connect with error: ${error.message}`);
+  .catch((connectionError) =>
+    error(`Failed to connect with error: ${connectionError.message}`)
+  );
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/blogs", blogsRouter);
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
+// app.get("/api/blogs", (request, response) => {
+//   Blog.find({}).then((blogs) => {
+//     response.json(blogs);
+//   });
+// });
 
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
+// app.post("/api/blogs", (request, response) => {
+//   const blog = new Blog(request.body);
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+//   blog.save().then((result) => {
+//     response.status(201).json(result);
+//   });
+// });
 
 app.listen(PORT, () => {
   info(`Server running on port ${PORT}`);
