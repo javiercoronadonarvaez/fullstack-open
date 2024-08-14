aed33acd56f9363bf4b49511b332c931a7be58b1
javiercoronadonarvaez <j.coronado.narvaez@accenture.com>
2024-08-14 14:42:26 -0600

    Finished Exercise 4.2 from part4

---
+++ part4/blog/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { info, errorInfo } = require("./utils/logger");
+const { MONGODB_URI, PORT } = require("./utils/config");
+const mongoose = require("mongoose");
+const blogsRouter = require("./controllers/blogs");
+const middleware = require("./utils/middleware");
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) => info(`Successful connection to MongoDB`))
+  .catch((error) =>
+    errorInfo(`Failed to connect with error: ${error.message}`)
+  );
+
+app.use(cors());
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use("/api/blogs", blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
--- part4/blog/index.js
+++ part4/blog/index.js
@@ -1,27 +1,6 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const { info, errorInfo } = require("./utils/logger");
-const { MONGODB_URI, PORT } = require("./utils/config");
-const mongoose = require("mongoose");
-const blogsRouter = require("./controllers/blogs");
-const middleware = require("./utils/middleware");
-
-mongoose
-  .connect(MONGODB_URI)
-  .then((result) => info(`Successful connection to MongoDB`))
-  .catch((error) =>
-    errorInfo(`Failed to connect with error: ${error.message}`)
-  );
-
-app.use(cors());
-app.use(express.json());
-app.use(middleware.requestLogger);
-
-app.use("/api/blogs", blogsRouter);
-
-app.use(middleware.unknownEndpoint);
-app.use(middleware.errorHandler);
+const app = require("./app");
+const { info } = require("./utils/logger");
+const { PORT } = require("./utils/config");
 
 app.listen(PORT, () => {
   info(`Server running on port ${PORT}`);
