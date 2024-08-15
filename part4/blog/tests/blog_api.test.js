4afc3b1799636766d0832e29103e65a50bf23e75
javiercoronadonarvaez <j.coronado.narvaez@accenture.com>
2024-08-15 15:24:26 -0600

    Finished Exercise 4.8 from part4

---
+++ part4/blog/tests/blog_api.test.js
@@ -0,0 +1,34 @@
+const { test, after, describe, beforeEach } = require("node:test");
+const assert = require("node:assert");
+const supertest = require("supertest");
+const mongoose = require("mongoose");
+const helper = require("./test_helper");
+const app = require("../app");
+const api = supertest(app);
+
+const Blog = require("../models/blog");
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  console.log("After delete");
+
+  for (let blog of helper.initialBlogs) {
+    let newBlog = new Blog(blog);
+    await newBlog.save();
+  }
+});
+
+describe("Blog HTTP Testing", () => {
+  test.only("GET call retrieves the correct amount of blogs in JSON format", async () => {
+    const notes = await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+
+    assert.strictEqual(notes.body.length, helper.initialBlogs.length);
+  });
+});
+
+after(async () => {
+  await mongoose.connection.close();
+});
