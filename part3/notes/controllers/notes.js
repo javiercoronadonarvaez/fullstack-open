bb91bc8134893fee29e5f93fe0590c2a4207802e
javiercoronadonarvaez <j.coronado.narvaez@accenture.com>
2024-08-16 12:23:05 -0600

    Finished lesson on Populating users and notes from User Administration

--- part3/notes/controllers/notes.js
+++ part3/notes/controllers/notes.js
@@ -1,20 +1,42 @@
 const notesRouter = require('express').Router()
+const bcrypt = require('bcrypt')
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async (request, response) => {
-  const notes = await Note.find({})
+  const notes = await Note.find({}).populate('user', { username: 1, name: 1 })
   response.json(notes)
 })
 
 notesRouter.post('/', async (request, response) => {
   const body = request.body
 
+  let user = await User.findById(body.userId)
+
+  if (!user) {
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash('password', saltRounds)
+    user = new User({
+      username: 'newtester',
+      name: 'New Tester',
+      passwordHash: passwordHash,
+    })
+  }
+
+  console.log('User info: ', user)
+
   const note = new Note({
     content: body.content,
-    important: body.important || false,
+    important: body.important === undefined ? false : body.important,
+    user: user.id,
   })
 
   const savedNote = await note.save()
+  //user.notes = user.notes.concat(savedNote._id);
+  user.notes = user.notes.concat(savedNote.id)
+  //user.notes = user.notes.concat(savedNote.toJSON().id);
+  await user.save()
+
   response.status(201).json(savedNote)
 })
 
