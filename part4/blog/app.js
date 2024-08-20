39e9d95697897fef112bfc18993312aedf3f83ec
javiercoronadonarvaez <j.coronado.narvaez@accenture.com>
2024-08-20 17:11:38 -0600

    Finished Exercises 4.18 and 4.19 from part4

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -7,6 +7,7 @@ const { MONGODB_URI } = require('./utils/config')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 
 mongoose
@@ -20,6 +21,7 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 
