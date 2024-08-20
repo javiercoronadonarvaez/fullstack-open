e5f74f333c43dca7c7b55bee0be7c182f312f28c
javiercoronadonarvaez <j.coronado.narvaez@accenture.com>
2024-08-20 16:49:37 -0600

    Finished Exercise 4.17 from part4

--- part4/blog/controllers/users.js
+++ part4/blog/controllers/users.js
@@ -3,11 +3,12 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-  //   const users = await User.find({}).populate('notes', {
-  //     content: 1,
-  //     important: 1,
-  //   })
-  const users = await User.find({})
+  const users = await User.find({}).populate('blogs', {
+    title: 1,
+    author: 1,
+    url: 1,
+    likes: 1,
+  })
 
   response.json(users)
 })
