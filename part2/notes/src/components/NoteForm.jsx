53acdc222813a5745cf05f5e1e2a7c6a4451fc97
javiercoronadonarvaez <j.coronado.narvaez@accenture.com>
2024-08-23 16:57:05 -0600

    Finished Testing React Apps lesson from part5

--- part2/notes/src/components/NoteForm.jsx
+++ part2/notes/src/components/NoteForm.jsx
@@ -18,11 +18,15 @@ const NoteForm = ({ createNote }) => {
   }
 
   return (
-    <div>
+    <div className="formDiv">
       <h2>Create a new note</h2>
 
       <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+          placeholder="write note content here"
+        />
         <button type="submit">save</button>
       </form>
     </div>
