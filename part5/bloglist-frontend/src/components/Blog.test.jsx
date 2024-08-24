9f20eb17ea6d5f4360321c89fde18dfcb569ef3b
javiercoronadonarvaez <j.coronado.narvaez@accenture.com>
2024-08-23 18:22:42 -0600

    Finished Exercise 5.15 from part5

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -5,14 +5,20 @@ import { beforeEach } from 'vitest'
 
 describe('<Blog />', () => {
   let container
+
   const blog = {
     author: 'Javier Coronado',
     title: 'Test Blog',
     url: 'www.test.com',
     likes: 5,
   }
+
+  let incrementLike = vi.fn()
+
   beforeEach(() => {
-    container = render(<Blog blog={blog} />).container
+    container = render(
+      <Blog blog={blog} incrementLikeCount={incrementLike} />
+    ).container
   })
 
   test('Check component displaying a blog renders blog title and author BUT NOT URL and likes', () => {
@@ -44,5 +50,36 @@ describe('<Blog />', () => {
     expect(div).not.toHaveStyle('display: none')
     expect(urlPresent).toBe(true)
     expect(likesPresent).toBe(true)
+  })
+
+  test('Check component displaying a blog renders blog title and author BUT NOT URL and likes', () => {
+    const div = container.querySelector('.blogShowLimited')
+    screen.debug(div)
+
+    const titlePresent = div.textContent.includes('Test Blog')
+    const authorPresent = div.textContent.includes('Javier Coronado')
+    const urlNotPresent = div.textContent.includes('www.test.com')
+    const likesNotPresent = div.textContent.includes(5)
+
+    expect(div).not.toHaveStyle('display: none')
+    expect(titlePresent).toBe(true)
+    expect(authorPresent).toBe(true)
+    expect(urlNotPresent).toBe(false)
+    expect(likesNotPresent).toBe(false)
+  })
+
+  test('Check that if like button is clicked twice, event handler is called twice', async () => {
+    const user = userEvent.setup()
+    const viewButton = screen.getByText('view')
+    await user.click(viewButton)
+
+    const div = container.querySelector('.blogShowAll')
+
+    const increasedNumLikesButton = screen.getByText('like')
+    await user.click(increasedNumLikesButton)
+    await user.click(increasedNumLikesButton)
+
+    expect(div).not.toHaveStyle('display: none')
+    expect(incrementLike.mock.calls).toHaveLength(2)
   })
 })
