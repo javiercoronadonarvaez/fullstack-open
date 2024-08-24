c996a6efa48744badc00ac003d4f05d3cceff38c
javiercoronadonarvaez <j.coronado.narvaez@accenture.com>
2024-08-23 18:14:09 -0600

    Finished Exercise 5.14 from part5

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,29 +1,48 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
+import { beforeEach } from 'vitest'
 
-test('Check component displaying a blog renders blog title and author BUT NOT URL and likes', () => {
+describe('<Blog />', () => {
+  let container
   const blog = {
     author: 'Javier Coronado',
     title: 'Test Blog',
     url: 'www.test.com',
     likes: 5,
   }
+  beforeEach(() => {
+    container = render(<Blog blog={blog} />).container
+  })
 
-  let container
+  test('Check component displaying a blog renders blog title and author BUT NOT URL and likes', () => {
+    const div = container.querySelector('.blogShowLimited')
+    screen.debug(div)
 
-  container = render(<Blog blog={blog} />).container
+    const titlePresent = div.textContent.includes('Test Blog')
+    const authorPresent = div.textContent.includes('Javier Coronado')
+    const urlNotPresent = div.textContent.includes('www.test.com')
+    const likesNotPresent = div.textContent.includes(5)
 
-  const div = container.querySelector('.blogShowLimited')
-  screen.debug(container)
+    expect(div).not.toHaveStyle('display: none')
+    expect(titlePresent).toBe(true)
+    expect(authorPresent).toBe(true)
+    expect(urlNotPresent).toBe(false)
+    expect(likesNotPresent).toBe(false)
+  })
 
-  const titlePresent = div.textContent.includes('Test Blog')
-  const authorPresent = div.textContent.includes('Javier Coronado')
-  const urlNotPresent = div.textContent.includes('www.test.com')
-  const likesNotPresent = div.textContent.includes(5)
+  test('Check component displaying a blog renders blog, title, author and likes when button is clicked', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
 
-  expect(titlePresent).toBe(true)
-  expect(authorPresent).toBe(true)
-  expect(urlNotPresent).toBe(false)
-  expect(likesNotPresent).toBe(false)
+    const div = container.querySelector('.blogShowAll')
+
+    const urlPresent = div.textContent.includes('www.test.com')
+    const likesPresent = div.textContent.includes(5)
+
+    expect(div).not.toHaveStyle('display: none')
+    expect(urlPresent).toBe(true)
+    expect(likesPresent).toBe(true)
+  })
 })
