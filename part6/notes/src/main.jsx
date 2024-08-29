3c98d93029143319099bf59647ec304ecb72f712
javiercoronadonarvaez <j.coronado.narvaez@accenture.com>
2024-08-28 19:21:26 -0600

    Finished Flux architecture and Redux lesson

--- part6/notes/src/main.jsx
+++ part6/notes/src/main.jsx
@@ -1,33 +1,15 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
 import App from "./App";
 import noteReducer from "./reducers/noteReducer";
-import { createStore } from "redux";
 
 const store = createStore(noteReducer);
 
-store.dispatch({
-  type: "NEW_NOTE",
-  payload: {
-    content: "the app state is in redux store",
-    important: true,
-    id: 1,
-  },
-});
-
-store.dispatch({
-  type: "NEW_NOTE",
-  payload: {
-    content: "state changes are made with actions",
-    important: false,
-    id: 2,
-  },
-});
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-const renderApp = () => {
-  root.render(<App store={store} />);
-};
-
-renderApp();
-store.subscribe(renderApp);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
