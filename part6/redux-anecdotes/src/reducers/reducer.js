a6e81fda713440cbae4e793280345302da837638
javiercoronadonarvaez <j.coronado.narvaez@accenture.com>
2024-08-30 10:32:00 -0600

    Refactored code with reducer.js file

---
+++ part6/redux-anecdotes/src/reducers/reducer.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import anecdoteReducer from "./anecdoteReducer";
+import filterReducer from "./filterReducer";
+
+const store = configureStore({
+  reducer: { anecdotes: anecdoteReducer, filter: filterReducer },
+});
+
+export default store;
