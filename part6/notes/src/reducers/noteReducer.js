c6053e906909c842370fcb5111f937445b9fd2c1
javiercoronadonarvaez <j.coronado.narvaez@accenture.com>
2024-09-02 13:43:48 -0600

    Finished Communicating with server in a Redux application lesson

--- part6/notes/src/reducers/noteReducer.js
+++ part6/notes/src/reducers/noteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice, current } from "@reduxjs/toolkit";
+import notesService from "../services/notes";
 
 const initialState = [
   {
@@ -19,9 +20,6 @@ const noteSlice = createSlice({
   name: "notes",
   initialState: [],
   reducers: {
-    createNote(state, action) {
-      state.push(action.payload);
-    },
     toggleImportanceOf(state, action) {
       const id = action.payload;
       const noteToChange = state.find((n) => n.id === id);
@@ -41,6 +39,20 @@ const noteSlice = createSlice({
   },
 });
 
-export const { createNote, toggleImportanceOf, appendNote, setNotes } =
-  noteSlice.actions;
+export const { toggleImportanceOf, setNotes, appendNote } = noteSlice.actions;
+
+export const initializeNotes = () => {
+  return async (dispatch) => {
+    const notes = await notesService.getAll();
+    dispatch(setNotes(notes));
+  };
+};
+
+export const createNote = (content) => {
+  return async (dispatch) => {
+    const newNote = await notesService.createNew(content);
+    dispatch(appendNote(newNote));
+  };
+};
+
 export default noteSlice.reducer;
