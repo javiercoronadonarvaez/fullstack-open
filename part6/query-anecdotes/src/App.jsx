6eac611789558a57dce17d985b0b7767d53d4bc6
javiercoronadonarvaez <j.coronado.narvaez@accenture.com>
2024-09-02 19:07:58 -0600

    Finished Exercises 6.21 and 6.22 from part6

---
+++ part6/query-anecdotes/src/App.jsx
@@ -0,0 +1,62 @@
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getAnecdotes, updateAnecdoteWithVote } from "../requests";
+import AnecdoteForm from "./components/AnecdoteForm";
+import Notification from "./components/Notification";
+
+const App = () => {
+  const queryClient = useQueryClient();
+  const votedAnecdoteMutation = useMutation({
+    mutationFn: updateAnecdoteWithVote,
+    onSuccess: (updatedAnecdote) => {
+      const anecdotes = queryClient.getQueryData(["anecdotes"]);
+      const updatedAnecdotes = anecdotes.map((anecdote) =>
+        anecdote.id === updatedAnecdote.id ? updatedAnecdote : anecdote
+      );
+      queryClient.setQueryData(["anecdotes"], updatedAnecdotes);
+    },
+  });
+
+  const handleVote = (anecdote) => {
+    console.log("vote");
+    votedAnecdoteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 });
+  };
+
+  const result = useQuery({
+    queryKey: ["anecdotes"],
+    queryFn: getAnecdotes,
+    retry: false,
+  });
+
+  if (result.status === "error") {
+    return (
+      <span>Anecdote service note available due to problems in server</span>
+    );
+  }
+
+  if (result.isLoading) {
+    return <div>loading data...</div>;
+  }
+
+  const anecdotes = result.data;
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+
+      <Notification />
+      <AnecdoteForm />
+
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
---
+++ part6/query-anecdotes/src/components/Notification.jsx
@@ -0,0 +1,13 @@
+const Notification = () => {
+  const style = {
+    display: "none",
+    border: "solid",
+    padding: 10,
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return <div style={style}></div>;
+};
+
+export default Notification;
