8b4115f29afbf1e48128dd1a9929d8fb84975d19
javiercoronadonarvaez <j.coronado.narvaez@accenture.com>
2024-09-21 15:40:08 -0600

    Finished Login and Updating the cache lesson from part8

--- part8/phonebook-frontend/src/App.jsx
+++ part8/phonebook-frontend/src/App.jsx
@@ -1,13 +1,16 @@
-import { useQuery } from "@apollo/client";
+import { useQuery, useApolloClient } from "@apollo/client";
 import { useState } from "react";
 import { ALL_PERSONS } from "./queries";
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
 import PhoneForm from "./components/PhoneForm";
 import Notification from "./components/Notification";
+import LoginForm from "./components/LoginForm";
 
 const App = () => {
+  const [token, setToken] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
+  const client = useApolloClient();
 
   const result = useQuery(ALL_PERSONS);
 
@@ -22,9 +25,26 @@ const App = () => {
     }, 10000);
   };
 
+  const logout = () => {
+    setToken(null);
+    localStorage.clear();
+    client.resetStore();
+  };
+
+  if (!token) {
+    return (
+      <>
+        <Notification errorMessage={errorMessage} />
+        <h2>Login</h2>
+        <LoginForm setToken={setToken} setError={notify} />
+      </>
+    );
+  }
+
   return (
     <div>
       <Notification errorMessage={errorMessage} />
+      <button onClick={logout}>logout</button>
       <PersonForm setError={notify} />
       <PhoneForm setError={notify} />
       <Persons persons={result.data.allPersons} />
