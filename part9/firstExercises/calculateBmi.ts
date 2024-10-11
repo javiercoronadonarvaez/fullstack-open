698f98ad65984ac80752fc69d852e42a0e1c7452
javiercoronadonarvaez <j.coronado.narvaez@accenture.com>
2024-10-11 14:05:16 -0600

    Created part9 directory and finished Exercise 9.1

---
+++ part9/firstExercises/calculateBmi.ts
@@ -0,0 +1,65 @@
+interface MultiplyValues {
+  height: number;
+  weight: number;
+}
+
+const parseArguments = (args: string[]): MultiplyValues => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+  if (args.length > 4) throw new Error("Too many arguments");
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3]),
+    };
+  } else {
+    throw new Error("Provided values were not numbers!");
+  }
+};
+
+type BMI =
+  | "Underweight (Severe thinness)"
+  | "Underweight (Moderate thinness)"
+  | "Underweight (Mild thinness)"
+  | "Normal range"
+  | "Overweight (Pre-obese)"
+  | "Obese (Class I)"
+  | "Obese (Class II)"
+  | "Obese (Class III)";
+
+const calculateBmi = (height: number, weight: number): BMI => {
+  const bmi = weight / (height / 100) ** 2;
+  console.log("BMI", bmi);
+  switch (true) {
+    case bmi > 0 && bmi < 16:
+      return "Underweight (Severe thinness)";
+    case bmi >= 16 && bmi <= 16.9:
+      return "Underweight (Moderate thinness)";
+    case bmi >= 17 && bmi <= 18.4:
+      return "Underweight (Mild thinness)";
+    case bmi >= 18.5 && bmi <= 24.9:
+      return "Normal range";
+    case bmi >= 25 && bmi <= 29.9:
+      return "Overweight (Pre-obese)";
+    case bmi >= 30 && bmi <= 34.9:
+      return "Obese (Class I)";
+    case bmi >= 35 && bmi <= 39.9:
+      return "Obese (Class II)";
+    case bmi >= 40:
+      return "Obese (Class III)";
+    default:
+      throw new Error("Outside of range");
+  }
+};
+
+try {
+  // const { height, weight } = parseArguments(process.argv);
+  // console.log(calculateBmi(height, weight));
+  console.log(calculateBmi(180, 74));
+} catch (error: unknown) {
+  let errorMessage = "Something bad happened.";
+  if (error instanceof Error) {
+    errorMessage += " Error: " + error.message;
+  }
+  console.log(errorMessage);
+}
