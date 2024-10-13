1e4cf230d18d3e9143270323f454664cccc71eb4
javiercoronadonarvaez <j.coronado.narvaez@accenture.com>
2024-10-13 11:21:03 -0600

    Completed second lesson from Typing and Express app

--- part9/ilaris-flight-diaries/src/services/diaryService.ts
+++ part9/ilaris-flight-diaries/src/services/diaryService.ts
@@ -1,9 +1,14 @@
 import diaries from "../../data/entries";
 
-import { NonSensitiveDiaryEntry, DiaryEntry } from "../types";
+import { NewDiaryEntry, NonSensitiveDiaryEntry, DiaryEntry } from "../types";
 
 const getEntries = (): DiaryEntry[] => {
   return diaries;
+};
+
+const findById = (id: number): DiaryEntry | undefined => {
+  const entry = diaries.find((d) => d.id === id);
+  return entry;
 };
 
 const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
@@ -15,12 +20,19 @@ const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
   }));
 };
 
-const addDiary = () => {
-  return null;
+const addDiary = (entry: NewDiaryEntry): DiaryEntry => {
+  const newDiaryEntry = {
+    id: Math.max(...diaries.map((d) => d.id)) + 1,
+    ...entry,
+  };
+
+  diaries.push(newDiaryEntry);
+  return newDiaryEntry;
 };
 
 export default {
   getEntries,
   addDiary,
   getNonSensitiveEntries,
+  findById,
 };
